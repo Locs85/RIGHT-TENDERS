a196382019d507ac7a0631f2d200d9915915df72
Locs85 <alexbaroyan@gmail.com>
2020-07-24 17:41:45 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,48 @@
+import * as $ from 'jquery';
+import 'bootstrap';
+import 'popper.js';
+import 'owl.carousel';
+import 'jquery.scrollto';
+
+$(document).ready(function() {
+
+     
+     /*QUOTES CAROUSEL*/
+    $('.quotes__carousel').owlCarousel({
+    	 items: 4,
+	  	 loop: true,
+	     autoplay: false,
+	     nav: false,
+	     dots: false,
+	     slideBy: 1,
+	     margin: 10,
+	     responsive: {
+	     	 1200: {
+	     	 	items: 3
+	     	 },
+	     	 992: {
+	     	 	items: 3
+	     	 },
+	     	 768: {
+	     	 	items: 2
+	     	 },
+	     	 576: {
+	     	 	items: 1
+	     	 },
+	     	 0: {
+	     	 	items: 1
+	     	 }
+	     }
+    }); 
+     /*QUOTES CAROUSEL*/ 
+
+     /*SCROLL TO / LOOK MORE*/
+     $('a[data-link]').on('click', function() {
+     	  var id = $(this).attr('data-target');
+     	  $(window).scrollTo(id, 'slow', {
+     	  	 offset: -20
+     	  });
+     })
+     /*SCROLL TO / LOOK MORE*/ 
+
+});
